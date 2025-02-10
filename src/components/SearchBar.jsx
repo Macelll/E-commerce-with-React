14d5c0c3c051034a94bac06d64e741288b5bd1d3
Macelll <72009236+Macelll@users.noreply.github.com>
2025-02-10 23:47:39 +0800

    Globalize Search Bar
    Trying to do it but failed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import { useLocation } from 'react-router-dom';
 
-export const SearchBar = () => {
-    const {search, setSearch, showSearch, setShowSearch} = React.useContext(ShopContext);
+const SearchBar = () => {
+    const { search, setSearch, showSearch, setShowSearch } = React.useContext(ShopContext);
     const [visible, setVisible] = React.useState(false);
     const location = useLocation();
 
     useEffect(() => {
-        if (location.pathname.includes('/collection')){
+        if (location.pathname.includes('/collection')) {
             setVisible(true);
-        }else{
+        } else {
             setVisible(false);
         }
     }, [location]);
 
-  return showSearch && visible ?(
-    <div className='border-t border-b bg-gray-50 text-center'>
-        <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
-            <input value={search} onChange={(e)=>setSearch(e.target.value)}  className='flex-1 outline-none bg-inherit text-sm py-2' type="text" placeholder='Search' />
-            <img className='w-4 ' src={assets.search_icon} alt="Search Icon" />
+    return showSearch && visible ? (
+        <div className='border-t border-b bg-gray-50 text-center'>
+            <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
+                <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm py-2' type="text" placeholder='Search' />
+                <img className='w-4' src={assets.search_icon} alt="Search Icon" />
+            </div>
+            <img onClick={() => setShowSearch(false)} className='inline w-3 cursor-pointer' src={assets.cross_icon} alt="Close Icon" />
         </div>
-        <img onClick={()=>setShowSearch(false)} className='inline w-3 cursor-pointer' src={assets.cross_icon} alt="Close Icon" />
-    </div>
-  ) : null
+    ) : null;
 }
+
+export default SearchBar;
